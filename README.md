f0cf871a1eadc2540d45036af7f96457d2015cae
Charles <csipe84@gmail.com>
2022-02-16 20:28:53 -0800

    change console log to alert

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 
-#LAB - 2 
+# LAB - 2 
 
-##About Me Quiz
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+## About Me Quiz
+>I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
 
-##Author: Charles Sipe/Group Name
+## Author: Charles Sipe/Group Name
 
-##Links and Resources
+## Links and Resources
 
-##submission PR
+## submission PR
 
-##Any Links you used as reference
+## Any Links you used as reference
 
-##Reflections and Comments
-Consider including the answers to your daily journal and submission questions here
+## Reflections and Comments
+>Consider including the answers to your daily journal and submission questions here
 This is also a good place to reflect on the tools and resources used and learned
--- js/app.js
+++ js/app.js
@@ -5,40 +5,43 @@ let username = prompt('What is your name?');
 alert("Hello " + username + "!");
 
 let question1 = prompt('Was my first job after graduation an accounting clerk?').toLowerCase();
-let question2 = prompt('Have I ever worked part time at the Seattle Sonics Team Shop at Key Arena?').toLowerCase();
-let question3 = prompt('Have I ever lived in Oklahoma?').toLowerCase();
-let question4 = prompt('Have I ever been fired?').toLowerCase();
-let question5 = prompt('Have I ever commuted over 25 miles to work?').toLowerCase();
-
 
 if (question1 === 'yes' || question1 === 'y') {
-  console.log('Yes is correct!');
+  alert('Yes is correct!');
 } else {
-  console.log('Sorry, no is not the correct response.');
+  alert('Sorry, no is not the correct response.');
 }
+
+let question2 = prompt('Have I ever worked part time at the Seattle Sonics Team Shop at Key Arena?').toLowerCase();
 
 if (question2 === 'yes' || question2 === 'y') {
-    console.log('You\'re right!');
+  alert('You\'re right!');
 } else {
-  console.log('Sorry, that is incorrect');
+  alert('Sorry, that is incorrect');
 }
+
+let question3 = prompt('Have I ever lived in Oklahoma?').toLowerCase();
 
 if (question3 === 'yes' || question3 === 'y') {
-    console.log('No, it was actually Texas, not Oklahoma.');
+  alert('No, it was actually Texas, not Oklahoma.');
 } else {
-  console.log('Yes, that is right.');
+  alert('Yes, that is right.');
 }
+
+let question4 = prompt('Have I ever been fired?').toLowerCase();
 
 if (question4 === 'yes' || question4 === 'y') {
-    console.log('No, I have not.');
+  alert('No, I have not.');
 } else {
-  console.log('Yes, that is right.');
+  alert('Yes, that is right.');
 }
 
+let question5 = prompt('Have I ever commuted over 25 miles to work?').toLowerCase();
+
 if (question5 === 'yes' || question5 === 'y') {
-    console.log('Yep, it was brutal.');
+  alert('Yep, it was brutal.');
 } else {
-  console.log('Actually, I have.');
+  alert('Actually, I have.');
 }
 
 alert("Thanks for playing " + username + "!");
