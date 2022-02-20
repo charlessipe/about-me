52220bb0e5e7bdb3ebc0c041720d56095e493cbc
Charles <csipe84@gmail.com>
2022-02-19 16:32:44 -0800

    guess name working

--- js/app.js
+++ js/app.js
@@ -65,6 +65,26 @@ if (attemptcounter ===4){
     alert("You have exhausted your 4 attempts. The answer is 5.")
 }
 
+let favoriteWebsites = ['cal newport', 'css tricks'];
+
+let trys = 6;
+let trycounter = 0;
+
+for (let j = 0; j < trys; j++){
+    let question7 = prompt('What is one of my favorite websites that start with C?').toLowerCase();
+
+    if (question7 === favoriteWebsites[0] || question7 === favoriteWebsites[1]) {
+      alert('Correct. Acceptable answers are ' + favoriteWebsites[0] + " and " + favoriteWebsites[1]);
+      trys = 0;
+    } else {
+      alert('Not quite. Remaining tries: ' + (trys - j - 1));
+      trycounter++;
+    }
+}
+
+if (trycounter ===6){
+    alert("You are out of attempts. Acceptable answers are " + favoriteWebsites[0] + " and " + favoriteWebsites[1]);
+}
 
 
 /*
